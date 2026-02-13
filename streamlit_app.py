b0a155bac879dbd32f73ae4f6d26a37493768e83
EdwinGoh1 <edwingohworkschool@gmail.com>
2026-02-14 03:24:16 +0800

    🍻Add requirements.txt and streamlit_app.py from LMS

---
+++ streamlit_app.py
@@ -0,0 +1,71 @@
+import joblib
+import streamlit_app as st
+import numpy as np
+import pandas as pd
+
+## Load trained model
+model = joblib.load("models/depression_rf_model.pkl")
+
+## Streamlit app
+st.title("Depression Prediction")
+
+## Define the input options
+
+
+
+## User inputs
+town_selected = st.selectbox("Select Town", towns)
+flat_type_selected = st.selectbox("Select Flat Type", flat_types)
+storey_range_selected = st.selectbox("Select Storey", storey_ranges)
+floor_area_selected = st.slider("Select Floor Area (sqm)", 
+                                min_value=30, 
+                                max_value=200, 
+                                value=70)
+
+## Predict button
+if st.button("Predict HDB price"):
+
+    ## Create dict for input features
+    input_data = {
+        'town': town_selected,
+        'flat_type': flat_type_selected,
+        'storey_range': storey_range_selected,
+        'floor_area': floor_area_selected
+    }
+
+    ## Convert input data to a DataFrame
+    df_input = pd.DataFrame({
+        'town': [town_selected],
+        'flat_type': [flat_type_selected],
+        'storey_range': [storey_range_selected],
+        'floor_area': [floor_area_selected]
+    })
+
+    ## One-hot encoding
+    df_input = pd.get_dummies(df_input, 
+                              columns = ['town', 'flat_type', 'storey_range']
+                             )
+    
+    # df_input = df_input.to_numpy()
+
+    df_input = df_input.reindex(columns = model.feature_names_in_,
+                                fill_value=0)
+
+
+
+    ## Predict
+    y_unseen_pred = model.predict(df_input)[0]
+    st.success(f"Predicted Resale Price: ${y_unseen_pred:,.2f}")
+
+## Page design
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background: url("https://www.shutterstock.com/shutterstock/videos/1025418011/thumb/1.jpg");
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
